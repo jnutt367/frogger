caa47bb9e7aaa2c0028bafff5be0c3ae03f3e8f4
Jason Nutt <jnutt367@gmail.com>
2021-01-27 14:19:45 -0500

    Add files via upload

--- js/frogger.js
+++ js/frogger.js
@@ -1,1 +1,8 @@
-const frogger
+class Frogger {
+    constructor() {
+        this.spriteWidth = 250;
+        this.spriteHeight = 250;
+        this.width = this.spriteWidth / 5;
+        this.height = this.spriteHeight / 5;
+    }
+}
--- js/setup.js
+++ js/setup.js
@@ -24,3 +24,16 @@ canvas.width = 600;
 canvas.height = 600;
 
 // global variables
+
+const grid = 80;
+let keys = [];
+score = 0;
+let collisionsCount = 0;
+let frame = 0;
+let gameSpeed = 1;
+
+const particlesArray = [];
+const maxParticles = 300;
+const ripplesArray = [];
+const carsArray = [];
+const logsArray = [];
