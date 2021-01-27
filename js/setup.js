fb72d7cb9a8612203bd4ec7f2a16d35872eb9f1e
Jason Nutt <jnutt367@gmail.com>
2021-01-27 13:27:45 -0500

    Setup js folder structure

---
+++ js/setup.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById('canvas1');
+const ctx1 = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas = document.getElementById('canvas2');
+const ctx2 = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas = document.getElementById('canvas3');
+const ctx3 = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas = document.getElementById('canvas4');
+const ctx4 = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas = document.getElementById('canvas5');
+const ctx5 = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+// global variables
