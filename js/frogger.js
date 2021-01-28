1f16a6c599c27eef543b7070981fffdae4b44ab1
Jason Nutt <jnutt367@gmail.com>
2021-01-28 13:03:17 -0500

    Add files via upload
    There is a problem with the animate() inside of utilities.j but I need to take mu eyes off of it for a bit....I am currently at 14:08 in the Frank Labratory video on youTube. Revisit is emminent.

--- js/frogger.js
+++ js/frogger.js
@@ -4,5 +4,18 @@ class Frogger {
         this.spriteHeight = 250;
         this.width = this.spriteWidth / 5;
         this.height = this.spriteHeight / 5;
+        this.x = canvas.width/2 -this.width/2;
+        this.y = canvas.height - this.heigth - 40;
+        this.moving = false;
+        this.frameX = 0;
+        this.frameY = 0;
+    }
+    update() {
+        console.log('update');
+    }
+    draw() {
+        ctx3.fillstyle = "green";
+        ctx3.fillRect(this.x, this.y, this.height, this.width);
     }
 }
+const frogger = new Frogger();
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,8 @@
-const utilities
+function animate() {
+        ctx3.clearRect(0, 0, canvas.width, canvas.height);
+        frogger.draw();
+        frogger.update();
+        requestAnimationFrame(animate);
+}
+
+animate();
